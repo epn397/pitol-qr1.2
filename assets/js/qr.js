e6ece28b34cc95b79a656298823fe90f53f917c1
epn397 <pritambarua93@gmail.com>
2026-07-13 04:06:19 +0600

    Update qr.js

--- assets/js/qr.js
+++ assets/js/qr.js
@@ -1,47 +1,325 @@
-prepareData(
-type,
-value
-){
+/**
+ * PITOL QR PRO
+ * QR Generator Module
+ */
 
 
-switch(type){
+export class QRGenerator {
 
 
-case "wifi":
+    constructor(){
 
-return `
-WIFI:T:WPA;
-S:${value.ssid};
-P:${value.password};
-`;
-
+        this.preview =
+        document.getElementById(
+            "qrPreview"
+        );
 
 
-case "email":
-
-return `
-MATMSG:
-TO:${value.email};
-SUB:${value.subject};
-BODY:${value.body};
-`;
+        this.logo = null;
 
 
-
-case "phone":
-
-return `
-TEL:${value}
-`;
+        const logoInput =
+        document.getElementById(
+            "logoInput"
+        );
 
 
-
-default:
-
-return value;
+        if(logoInput){
 
 
-}
+            logoInput
+            .addEventListener(
+
+                "change",
+
+                event=>{
+
+
+                    const file =
+                    event.target.files[0];
+
+
+                    if(file){
+
+
+                        const reader =
+                        new FileReader();
+
+
+
+                        reader.onload = e=>{
+
+
+                            this.logo =
+                            e.target.result;
+
+
+                        };
+
+
+
+                        reader.readAsDataURL(
+                            file
+                        );
+
+
+                    }
+
+
+                }
+
+            );
+
+
+        }
+
+
+    }
+
+
+
+
+    prepareData(
+        type,
+        value
+    ){
+
+
+
+        switch(type){
+
+
+
+            case "url":
+
+
+                return value;
+
+
+
+
+            case "wifi":
+
+
+                return (
+
+`WIFI:T:WPA;
+S:${value};
+P:;
+;`
+
+                );
+
+
+
+
+
+            case "email":
+
+
+                return (
+
+`MATMSG:
+TO:${value};
+SUB:;
+BODY:;
+;`
+
+                );
+
+
+
+
+
+            case "phone":
+
+
+                return (
+
+`TEL:${value}`
+
+                );
+
+
+
+
+
+            case "vcard":
+
+
+                return (
+
+`BEGIN:VCARD
+VERSION:3.0
+FN:${value}
+END:VCARD`
+
+                );
+
+
+
+
+
+            default:
+
+
+                return value;
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+    generate(
+
+        type,
+
+        value,
+
+        options={}
+
+    ){
+
+
+
+        if(
+            !this.preview
+        ){
+
+            return;
+
+        }
+
+
+
+
+
+        this.preview.innerHTML="";
+
+
+
+
+
+        const data =
+
+        this.prepareData(
+
+            type,
+
+            value
+
+        );
+
+
+
+
+
+
+
+        const canvas =
+        document.createElement(
+            "canvas"
+        );
+
+
+
+        this.preview
+        .appendChild(
+            canvas
+        );
+
+
+
+
+
+
+        QRCode.toCanvas(
+
+            canvas,
+
+            data,
+
+            {
+
+                width:300,
+
+
+                margin:2,
+
+
+                color:{
+
+
+                    dark:
+                    options.dark ||
+                    "#000000",
+
+
+
+                    light:
+                    options.light ||
+                    "#ffffff"
+
+
+
+                }
+
+
+
+            },
+
+
+            error=>{
+
+
+                if(error){
+
+                    console.error(
+                        error
+                    );
+
+                }
+
+
+
+            }
+
+
+        );
+
+
+
+    }
+
+
+
+
+
+
+
+    clear(){
+
+
+        if(this.preview){
+
+
+            this.preview.innerHTML="";
+
+
+        }
+
+
+    }
+
+
 
 
 }
