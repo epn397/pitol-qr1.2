0aae7560d33cd118a9049d7482f529151b49cda3
epn397 <pritambarua93@gmail.com>
2026-07-13 02:08:50 +0600

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,40 @@
+/**
+ * PITOL QR PRO
+ * Application Configuration
+ */
+
+
+export const CONFIG = {
+
+
+    APP_NAME: "PITOL QR PRO",
+
+
+    VERSION: "1.0.0",
+
+
+    STORAGE_KEY: "pitol_qr_history",
+
+
+    THEME_KEY: "pitol_qr_theme",
+
+
+    MAX_HISTORY: 100,
+
+
+    QR_DEFAULTS: {
+
+        width: 300,
+
+        height: 300,
+
+        colorDark:"#000000",
+
+        colorLight:"#ffffff",
+
+        correctLevel:"H"
+
+    }
+
+
+};
